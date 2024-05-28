65019fff732ebf41cf23e1a5dab4a57a1224a0c0
cristalrapp05 <nerinacristal05@gmail.com>
2024-05-28 18:52:39 -0300

    Add files via upload

---
+++ student.controller.php
@@ -0,0 +1,17 @@
+<?php
+Include ("model/student.class.php");
+
+$student= new Student(); //creamos el objeto $student
+
+$operation=$_REQUEST["operation"];
+
+if($operation=="add"){
+
+}else if($operation=="update"){
+
+}else if($operation=="delete"){
+    
+}
+
+
+?>
